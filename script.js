6e4aa3218f030a1fcec8196884a36c75dfedaeb4
tomerikheggedal <92306093+tomerikheggedal@users.noreply.github.com>
2021-11-05 22:49:36 +0100

    Responsive

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 function changeColor() {
-    const colors = [
-      'lightBlue',
-      'red',
-      'grey',
-      'lightGreen',
-      'coral',
-      'crimson',
-      'DarkOliveGreen'
-    ];
-      var randomColor = () => colors[Math.floor(Math.random()*colors.length)];
-      document.getElementById('mydiv').style.backgroundColor = randomColor;
-      
-      const makeGradientString = (color1, color2) => `linear-gradient(45deg, ${color1} 0%, ${color2} 100%)`
-    
-    document.getElementById('mydiv').style.background = makeGradientString(randomColor(), randomColor());
-    
-    }
-    
-    
+  const colors = [
+    "lightBlue",
+    "AliceBlue",
+    "DeepPink",
+    "IndianRed",
+    "red",
+    "grey",
+    "lightGreen",
+    "coral",
+    "crimson",
+    "DarkOliveGreen",
+    "Aqua",
+    "AquaMarine",
+    "CadetBlue",
+    "Brown",
+    "CornflowerBlue",
+    "cyan",
+    "DarkCyan",
+    "Chartreuse",
+    "Gold",
+    "Indigo",
+    "lavender",
+    "LightSeaGreen",
+    "maroon",
+    "MediumVioletRed",
+    "OliveDrab",
+    "slateGrey",
+    "Tomato",
+    "yellowGreen",
+    "teal",
+    "plum",
+  ];
+  let randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+  let firstColor = randomColor();
+  let secondColor = randomColor();
+
+  const makeGradientString = (color1, color2) =>
+    `linear-gradient(45deg, ${color1} 0%, ${color2} 100%)`;
+
+  //let gradientStyle = makeGradientString(randomColor(), randomColor());
+
+  document.getElementById("colorContainer").style.background =
+    makeGradientString(firstColor, secondColor);
+  document.getElementById("firstColor").innerHTML = `${firstColor}`;
+  document.getElementById("secondColor").innerHTML = `${secondColor}`;
+}
+
+// document.getElementById("colorContainer").style.backgroundColor = randomColor;
