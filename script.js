a7dfe6340a002795aa1e25789fe303570bc8d7e7
tomerikheggedal <92306093+tomerikheggedal@users.noreply.github.com>
2023-12-02 13:49:08 +0100

    first commit

--- script.js
+++ script.js
@@ -1,20 +1,81 @@
+let firstColor;
+let secondColor;
+
+let firstColorPinned = false;
+let secondColorPinned = false;
+
+function toggleFirstPin() {
+  firstColorPinned = !firstColorPinned;
+  document.getElementById("firstColor").classList.toggle("pinned");
+  document.getElementById("firstColor").innerHTML = `${
+    firstColorPinned ? "<span class='pinned-open'>p</span>" : "<span></span>"
+  } ${firstColor}`;
+}
+
+function toggleSecondPin() {
+  secondColorPinned = !secondColorPinned;
+  document.getElementById("secondColor").classList.toggle("pinned");
+  document.getElementById(
+    "secondColor"
+  ).innerHTML = `${secondColor} - ${secondColorPinned}`;
+}
+
 function changeColor() {
-    const colors = [
-      'lightBlue',
-      'red',
-      'grey',
-      'lightGreen',
-      'coral',
-      'crimson',
-      'DarkOliveGreen'
-    ];
-      var randomColor = () => colors[Math.floor(Math.random()*colors.length)];
-      document.getElementById('mydiv').style.backgroundColor = randomColor;
-      
-      const makeGradientString = (color1, color2) => `linear-gradient(45deg, ${color1} 0%, ${color2} 100%)`
-    
-    document.getElementById('mydiv').style.background = makeGradientString(randomColor(), randomColor());
-    
-    }
-    
-    
+  const colors = [
+    "lightBlue",
+    "AliceBlue",
+    "DeepPink",
+    "IndianRed",
+    "red",
+    "grey",
+    "lightGreen",
+    "coral",
+    "crimson",
+    "DarkOliveGreen",
+    "Aqua",
+    "AquaMarine",
+    "CadetBlue",
+    "Brown",
+    "CornflowerBlue",
+    "cyan",
+    "DarkCyan",
+    "Chartreuse",
+    "Gold",
+    "Indigo",
+    "lavender",
+    "LightSeaGreen",
+    "maroon",
+    "MediumVioletRed",
+    "OliveDrab",
+    "slateGrey",
+    "Tomato",
+    "yellowGreen",
+    "teal",
+    "plum",
+  ];
+  let randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+  if (!firstColorPinned) {
+    firstColor = randomColor();
+  }
+
+  if (!secondColorPinned) {
+    secondColor = randomColor();
+  }
+
+  while (firstColor == secondColor) {
+    secondColor = randomColor();
+  }
+
+  const makeGradientString = (color1, color2) =>
+    `linear-gradient(45deg, ${color1} 0%, ${color2} 100%)`;
+
+  document.getElementById("colorContainer").style.background =
+    makeGradientString(firstColor, secondColor);
+  document.getElementById(
+    "firstColor"
+  ).innerHTML = `${firstColor} - ${firstColorPinned}`;
+  document.getElementById(
+    "secondColor"
+  ).innerHTML = `${secondColor} - ${secondColorPinned}`;
+}
